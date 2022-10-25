e7bb9acd74c8cce53da2aa8cb7a74879ec98657d
Nicolas Skatchkovsky <nicolas@nmslap007198.client.kcl.ac.uk>
2022-10-25 15:54:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# DECOLLE
+
+Deep Continuous Local Learning (DECOLLE) is a surrogate gradient (SG) 
+algorithm leveraging per-layer local errors for online learning of 
+deep SNNs, for which details can be found in reference
+[Kaiser et al., 2020](https://www.frontiersin.org/articles/10.3389/fnins.2020.00424/full). 
+
+Each layer of the SNN is associated to a fixed, random projection layer
+to compute the real-valued errors.
+
+This library is meant as an add-on to Intel's [Lava](https://github.com/lava-nc) framework. 
+It is based on the authors' own implementation, which can be found on
+their [GitHub repository](https://github.com/nmi-lab/decolle-public/tree/master). 
+
+
+## Dependencies
+`lava-dl` library (https://github.com/lava-nc/lava-dl/)
+
+## Getting Started
+[NMNIST digit classification](https://github.com/kclip/lava-decolle/blob/main/tutorials/nmnist/train.ipynb) tutorial
+
+## License
+
+This project is licensed under the GPLv3 License - see the [LICENSE.txt](LICENSE.txt) file for details
+
+### Author
+Nicolas Skatchkovsky ([nskat](https://github.com/nskat))
---
+++ loss.py
@@ -0,0 +1,51 @@
+# Copyright : (c) UC Regents, Emre Neftci
+# Licence : GPLv3
+
+from torch import Tensor
+from torch.nn.modules.loss import _Loss
+import torch
+
+"""This module provides a custom loss function to train models with DECOLLE."""
+
+
+def _regularization(reg: float, voltage: Tensor) -> Tensor:
+    return reg * torch.mean(torch.relu(voltage + .01)) \
+        + reg * 3e-3 * torch.mean(torch.relu(0.1 - torch.sigmoid(voltage)))
+
+
+class DECOLLELoss(_Loss):
+    """
+    Computes the DECOLLE Loss for the model, comprising the sum of the per-layer
+    local pseudo-losses, and a regularization term. Mathematically,
+    .. math::
+        L = \\sum_t \\sum_{l} \\ell_{t}^{l} +
+         \\text{reg} * \\left( \\langle \\text{reLu}(V_{i, t}^{l} + 0.01)
+          \\rangle_{i} + 0.003 * \\langle \\text{reLu}(0.1 - V_{i, t}^{l})
+           \\rangle_{i} \\right)
+    Parameters
+    ----------
+    loss_fn: object
+        local Pytorch loss function used for each layer
+    reg: float
+        strength of regulation
+    reduction: str
+        "mean" or "sum"
+    """
+
+    def __init__(self, loss_fn: _Loss, reg: float = 0, size_average=None,
+                 reduce=None, reduction: str = 'mean') -> None:
+        super(DECOLLELoss, self).__init__(size_average, reduce, reduction)
+        self.loss_fn = loss_fn(size_average, reduce, reduction)
+        self.reg = reg
+
+    def forward(self, readouts, voltages, target):
+        loss = 0
+        for r, v in zip(readouts, voltages):
+            for t in range(r.shape[-1]):
+                loss_t = self.loss_fn(r[..., t], target)
+                if self.reg > 0.:
+                    loss_t += _regularization(self.reg, v)
+                if self.reduction == 'mean':
+                    loss_t /= r.shape[-1]
+                loss += loss_t
+        return loss
